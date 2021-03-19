74f51a0426b42abc4bf22693e1cf10cd041e9fb0
Witalij Berdinskich <radio_rogal@keemail.me>
2021-03-19 23:35:07 +0200

    Add three command to upload a screenshot to x0.at; Remove Apache license.

--- swayshot.sh
+++ swayshot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Screenshot helper for sway.
+# Copyright 2017,2019,2021 Witalij Berdinskich.
 
 if [[ -z $WAYLAND_DISPLAY ]]; then
 	(>&2 echo Wayland is not running)
@@ -16,42 +18,68 @@ if [[ -z $SWAYSHOT_SCREENSHOTS ]]; then
 fi
 
 SCREENSHOT_TIMESTAMP=$(date "+${SWAYSHOT_DATEFMT:-%F_%H-%M-%S_%N}")
-
 SCREENSHOT_FULLNAME="$SWAYSHOT_SCREENSHOTS"/screenshot_${SCREENSHOT_TIMESTAMP}.png
 
-declare -r filter='
-# returns the focused node by recursively traversing the node tree
-def find_focused_node:
-    if .focused then . else (if .nodes then (.nodes | .[] | find_focused_node) else empty end) end;
-# returns a string in the format that grim expects
-def format_rect:
-    "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)";
-find_focused_node | format_rect
-'
+make_screenshot() {
+	case "$1" in
+		-h|--help)
+			echo 'Usage: swayshot [display|window|region]'
+			return 0
+			;;
+		region)
+			grim -g "$(slurp -b '#AFAFAFAF' -c '#FF3F3FAF' -s '#00000000' -w 3 -d)" "$2"
+			;;
+		window)
+			grim -g "$(swaymsg --type get_tree --raw | jq --raw-output "${filter}")" "$2"
+			;;
+		*)
+			grim -o "$(swaymsg --type get_outputs --raw | jq --raw-output '.[] | select(.focused) | .name')" "$2"
+			;;
+	esac
+}
 
-case "$1" in
-	-h|--help)
-		echo 'Usage: swayshot [display|window|region]'
-		return 0
-		;;
-	region)
-		grim -g "$(slurp -b '#AFAFAFAF' -c '#FF3F3FAF' -s '#00000000' -w 3 -d)" "$SCREENSHOT_FULLNAME"
-		;;		
-	window)
-		grim -g "$(swaymsg --type get_tree --raw | jq --raw-output "${filter}")" "$SCREENSHOT_FULLNAME"
+copy_to_clipboard() {
+	if type wl-copy >/dev/null  2>&1; then
+		echo -n "$1" | wl-copy
+	elif type xsel >/dev/null  2>&1; then
+		echo -n "$1" | xsel --clipboard
+	elif type xclip &>/dev/null; then
+		echo -n "$1" | xclip -selection clipboard
+	else
+		echo -n "$1"
+	fi
+}
+
+show_message() {
+	if type notify-send >/dev/null  2>&1; then
+		notify-send "$1"  --expire-time=3000 --category=screenshot --icon="$2"
+	fi
+}
+
+upload_screenshot() {
+	if [ -f "$1" ]; then
+		if type curl >/dev/null  2>&1; then
+			curl -F "file=@\"$1\";filename=.png" 'https://x0.at'
+		fi
+	fi
+}
+
+make_screenshot "$1" "$SCREENSHOT_FULLNAME"
+
+[[ ! -f "$SCREENSHOT_FULLNAME" ]] && return 1;
+
+case "$2" in
+	upload)
+		SCREENSHOT_LOCATOR=$(upload_screenshot "$SCREENSHOT_FULLNAME")
+		if [ -z "$SCREENSHOT_LOCATOR" ]; then
+			copy_to_clipboard "$SCREENSHOT_FULLNAME"
+			show_message "$SCREENSHOT_FULLNAME" document-save
+		else
+			copy_to_clipboard "$SCREENSHOT_LOCATOR"
+			show_message "$SCREENSHOT_LOCATOR" document-send
+		fi
 		;;
 	*)
-		grim -o "$(swaymsg --type get_outputs --raw | jq --raw-output '.[] | select(.focused) | .name')" "$SCREENSHOT_FULLNAME"
-		;;
+		copy_to_clipboard "$SCREENSHOT_FULLNAME"
+		show_message "$SCREENSHOT_FULLNAME" document-save
 esac
-
-if type wl-copy >/dev/null  2>&1; then
-	echo -n "$SCREENSHOT_FULLNAME" | wl-copy
-elif type xsel >/dev/null  2>&1; then
-	echo -n "$SCREENSHOT_FULLNAME" | xsel --clipboard
-elif type xclip &>/dev/null; then
-	echo -n "$SCREENSHOT_FULLNAME" | xclip -selection clipboard
-else
-	echo -n "$SCREENSHOT_FULLNAME"
-fi
-
